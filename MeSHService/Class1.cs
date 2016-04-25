cf2a0a2cbe60f84a034ddde224f434f5f12c9e9d
jpawlak <twojlech@gmail.com>
2016-04-26 00:26:20 +0200

    Reading MeSH dictionary to data structure classes generated from XML schema.
    Former-commit-id: ee85fa7e7a4db65592d969be404aa4bbe50aae0d

--- MeSHService/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿
-
-namespace MeSHService
-{
-    public class Class1
-    {
-    }
-}
---
+++ MeSHService/Xml/PathHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeSHService.Xml
+{
+    static class PathHelper
+    {
+        const string ProjectName = "MeSHService";
+
+        public static string GetProjectDirectoryPath()
+        {
+            string mainDirPath = Path.GetDirectoryName(
+                                    Path.GetDirectoryName(
+                                        Path.GetDirectoryName(Directory.GetCurrentDirectory())));
+
+            return Path.Combine(mainDirPath, ProjectName);
+        }
+    }
+}
