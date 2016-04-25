ce09e358ce40a25afd690c9d0c058323cadaa410
jpawlak <twojlech@gmail.com>
2016-04-26 00:53:23 +0200

    Reading MeSH dictionary - missing files
    Former-commit-id: f152855a5aa86719210faa4d9790bd8c42fbaac5

---
+++ MeSHService/Xml/XmlMeshReader.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace MeSHService.Xml
+{
+    public class XmlMeshReader
+    {
+        const string MeshDictionaryFilename = "desc2016.xml";
+
+        public DescriptorRecordSet Read()
+        {
+            string filePath = Path.Combine(PathHelper.GetProjectDirectoryPath(), MeshDictionaryFilename);
+            FileStream readStrem = new FileStream(filePath, FileMode.Open);
+
+            XmlSerializer meshSerializer = new XmlSerializer(typeof(DescriptorRecordSet));
+            DescriptorRecordSet set = meshSerializer.Deserialize(readStrem) as DescriptorRecordSet;
+
+            return set;
+        }
+    }
+}
